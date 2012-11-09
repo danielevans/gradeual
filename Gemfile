14cb644eb79a7645e379985a2b08671a6c208b0f
Daniel Evans <evans.daniel.n@gmail.com>
2012-11-09 08:22:02 -0700

    Initial rails commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'capistrano'
+gem 'jquery-rails'
+gem 'mysql2'
+gem 'rails', '3.2.8'
+gem 'unicorn'
+
+group :assets do
+  gem 'less-rails-bootstrap'
+  gem 'therubyracer'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'launchy'
+  gem 'factory_girl_rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gradeual::Application.config.secret_token = '0e025606077ec62fc8d55f1a706426f8d8cd1ef0c4904a15ac875801a885f80f7a0472425ddc4e468f468cbffec49ee58f49d636c8855b44469dd605296935cf'
