5d6ad183c2857446c2655ff8470bb34bc84a92d6
David Horne <dijit27@gmail.com>
2012-11-09 13:12:38 -0700

    Tests for problem

--- spec/models/problem_spec.rb
+++ spec/models/problem_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Problem do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
+  it { should validate_presence_of(:language) }
+  it { should ensure_inclusion_of(:language).in_array(App.supported_languages)}
 end
