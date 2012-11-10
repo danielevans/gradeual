8658bdd6fa643cbdc13c76377d257c223686c769
Daniel Evans <evans.daniel.n@gmail.com>
2012-11-10 06:26:38 -0700

    Configure default url for emails

--- config/app.rb
+++ config/app.rb
@@ -3,6 +3,7 @@ class App < Configurable # :nodoc:
   config.name = Rails.application.class.parent.name
   config.repo_root_path = File.join(Rails.root, 'uploads')
   # config.key = "value"
+  config.default_host = "gradeual.com"
   config.supported_languages = ["python27"]
   config.language_extensions = {
     "python27" => ".py"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,7 @@ Gradeual::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.sandbox_server = "localhost:8080"
+
+
+  config.action_mailer.default_url_options = { :host => App.default_host }
 end
