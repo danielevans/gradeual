2a8e094ccc09fe24d635db2e3b484d6b0eb3d6e6
David Horne <dijit27@gmail.com>
2012-11-09 20:16:32 -0700

    Refactor group for InheritedResources

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,43 +1,13 @@
-class GroupsController < ApplicationController
-  def index
-    @groups = Group.all
-  end
+class GroupsController < InheritedResources::Base
+  before_filter :authenticate_teacher!
 
-  def show
-    @group = Group.find(params[:id])
+  def beginning_of_association_chain
+    current_teacher
   end
-
-  def new
-    @group = Group.new
-    @group.teacher = current_teacher
-  end
-
+  
   def create
     @group = Group.new(params[:group])
-
-    if @group.save
-      redirect_to @group
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @group = Group.find(params[:id])
-  end
-
-  def update
-    @group = Group.find(params[:id])
-    if @group.update_attributes(params[:group])
-      redirect_to @group
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @group = Group.find(params[:id])
-    @group.destroy
-    redirect_to groups_path
+    @group.teacher = current_teacher
+    create!
   end
 end
