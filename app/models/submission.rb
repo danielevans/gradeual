ab7b804eeb21958ef5259db5fd49694b4463963d
Daniel Evans <evans.daniel.n@gmail.com>
2012-11-15 23:43:28 -0700

    Exit the success? function if response is nil

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -10,6 +10,7 @@ class Submission < ActiveRecord::Base
   after_save :store_code
 
   def success?
+    return true if responde.nil?
     JSON.parse(response).each do |status|
       if status['Success'] == false
         return false
