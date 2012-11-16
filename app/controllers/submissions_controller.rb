00afa4316cf08c42f15ecad63e2603ea77c715b9
Daniel Evans <evans.daniel.n@gmail.com>
2012-11-16 00:48:14 -0700

    more submission controller cleanup

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,6 +2,28 @@ require 'tmpdir'
 class SubmissionsController < InheritedResources::Base
   before_filter :authenticate_student!
 
+  def new
+    @submission = current_student.submissions.where(:assignment_id => params[:assignment_id]).first
+    new!
+  end
+
+  def create
+    create! do |format|
+      format.html {
+        redirect_to edit_resource_path(resource)
+      }
+    end
+  end
+
+
+  def update
+    update! do |format|
+      format.html {
+        redirect_to edit_resource_path(resource)
+      }
+    end
+  end
+
   def download_bundle
     path = GitFileStore.repo_path(current_student, resource.assignment.group)
     tar_name = "#{current_student.id}-#{resource.assignment.group.id}"
@@ -22,7 +44,7 @@ class SubmissionsController < InheritedResources::Base
     super
     if params[:assignment_id].present?
       @submission.assignment_id = params[:assignment_id]
-      @submission.code = @submission.assignment.problem.reference_code
+      @submission.code ||= @submission.assignment.problem.reference_code
     end
     @submission
   end
